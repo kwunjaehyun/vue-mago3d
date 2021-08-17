de510e3db48deae8ecc45ed78b7c512224063ae0
kwunjaehyun <shinekill3@gmail.com>
2021-08-17 19:59:25 +0900

    colon

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
     publicPath : '/',
     chainWebpack: config => {
         let imgRule = config.module.rule('images');
-        imgRule.store.set('test', /\.(png|PNG|jpe?g|JPE?G|gif|webp)(\?.*)?$/)
+        imgRule.store.set('test', /\.(png|PNG|jpe?g|JPE?G|gif|webp)(\?.*)?$/);
     }
 }
